1bfc398649cc69ab80c7696e9d750046ad0966d7
gregoryokon <gregory.okon@pau.edu.ng>
2025-10-12 11:32:41 -0500

    (my second commit)

---
+++ week-2/practice_1part2.rs
@@ -0,0 +1,14 @@
+fn main ()
+{
+	println!("welcome to COS 101");
+
+	println!("the course learning outcome is a as follows:");
+
+	println!(" 1. distinguish between computer and computer programming ,/n
+		       2. develop some techniques in computer science,/n
+               3. understand the different areas of study incomputer,/n
+               4. conversant with the applications of computer science ,/n
+               5. navigate the career prospects in computer science ,/n
+               6. conversant with computer programming concepts. ");
+
+}
